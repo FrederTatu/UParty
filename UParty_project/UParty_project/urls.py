93764a6a1a8a886122237e1dc6a4a8afc75fa459
nagisus <97454944+sucrosexd@users.noreply.github.com>
2025-12-21 23:33:16 +0400

    Delete UParty_project directory

--- UParty_project/UParty_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-# Здесь мы импортируем функцию определения рендера страницы по переходу пользователя по ссылке из файла views.py, чтобы применить ее здесь
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # Здесь мы соответственно указываем путь, по которому будет направлен пользователь. '' - стандартное определение главной (основной) страницы. Файл index.html указан в этой функции в файле views.py
-    path('events/',include('events.urls'))
-]
